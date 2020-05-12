8c585c0fc4ee32db2f9bcc73cbd0c637896ee26d
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-05-13 00:59:51 +0200

    setup.py: remove option encoding

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from os import path
 #         return f.read()
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(name='dameopencv',
